ca494158fd2b3d224eb19f8a6734fd0487387794
yueshenghui <1139590218@qq.com>
2020-03-16 10:25:30 +0800

    update

---
+++ js/page1/page1.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+    var page1 = {
+        _content: ''
+    }
+    page1.pageInit = function (content) {
+        page1._content = content;
+        console.log($('.searech', page1._content))
+    }
+// })
